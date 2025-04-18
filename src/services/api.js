a0d2418a02a6324391fe533180e7335f403a4382
emrat <mirdjalaloff@mail.ru>
2025-04-18 19:34:13 +0500

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "99c77f78b6070018ce120710c2c7718d";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
